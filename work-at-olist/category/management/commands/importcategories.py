e0587308e18847ce454890d07d7e4b6c52486c8f
Denis Costa <deniscostadsc@gmail.com>
2017-05-21 17:43:33 -0300

    Adds csv parameter to command
    Adds csv files to test

--- work-at-olist/category/management/commands/importcategories.py
+++ work-at-olist/category/management/commands/importcategories.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.management.base import BaseCommand
 
 from category.models import Channel
@@ -6,7 +8,15 @@ from category.models import Channel
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('channel_name', nargs=1)
+        parser.add_argument('csv_file', nargs=1)
 
     def handle(self, *args, **options):
         Channel.objects.create(name=options['channel_name'][0])
         self.stdout.write(self.style.SUCCESS('supermarket created.'))
+
+        if not os.path.exists(options['csv_file'][0]):
+            self.stdout.write(
+                self.style.WARNING('csv file doesn\'t exist.'))
+            return
+
+        self.stdout.write(self.style.SUCCESS('Categories imported.'))
--- work-at-olist/category/tests/tests_commands.py
+++ work-at-olist/category/tests/tests_commands.py
@@ -10,17 +10,37 @@ class ImportCategories(TestCase):
         self.out = StringIO()
 
     def test_output_on_creation_when_importing_categories(self):
-        call_command('importcategories', 'supermarket', stdout=self.out)
+        call_command(
+            'importcategories', 'supermarket', '../tests/files/correct.csv',
+            stdout=self.out)
         self.assertIn('supermarket created.', self.out.getvalue())
 
     def test_save_channel_when_importing_categories(self):
         self.assertEquals(
             0, Channel.objects.filter(name='supermarket').count())
-        call_command('importcategories', 'supermarket', stdout=self.out)
+        call_command(
+            'importcategories', 'supermarket', '../tests/files/correct.csv',
+            stdout=self.out)
         self.assertEquals(
             1, Channel.objects.filter(name='supermarket').count())
 
     def test_save_channel_with_correct_name(self):
-        call_command('importcategories', 'another-vendor', stdout=self.out)
-        channel_count = Channel.objects.filter(name='another-vendor').count()
-        self.assertEquals(1, channel_count)
+        self.assertEquals(
+            0, Channel.objects.filter(name='another-vendor').count())
+        call_command(
+            'importcategories', 'another-vendor', '../tests/files/correct.csv',
+            stdout=self.out)
+        self.assertEquals(
+            1, Channel.objects.filter(name='another-vendor').count())
+
+    def test_save_category(self):
+        call_command(
+            'importcategories', 'supermarket', '../tests/files/correct.csv',
+            stdout=self.out)
+        self.assertIn('Categories imported', self.out.getvalue())
+
+    def test_non_existing_csv_file(self):
+        call_command(
+            'importcategories', 'supermarket', 'doesnt_exist.csv',
+            stdout=self.out)
+        self.assertIn('csv file doesn\'t exist', self.out.getvalue())
