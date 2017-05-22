ea30d22f0561de0d0fda27d81ae20634d55ba03b
Denis Costa <deniscostadsc@gmail.com>
2017-05-22 14:33:59 -0300

    Adds some documentation to the endpoint

--- work-at-olist/category/views.py
+++ work-at-olist/category/views.py
@@ -5,5 +5,9 @@ from .serializers import ChannelSerializer
 
 
 class ChannelViewSet(viewsets.ModelViewSet):
+    """
+    list:
+    Return a list of all the existing users.
+    """
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
